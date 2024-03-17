13da540740b0bfaa191956b4a859fd9113547201
gegadiasamidze <gegadiasamidze2011@gmail.com>
2024-03-17 15:29:52 +0400

    commit

--- homeworks/Day12/lesson.py
+++
@@ -1,9 +0,0 @@
-user_mark= input (რა მიიღე ნიშანი ? )
-mark = int (input (user_mark + teacher say))
-
-teacher say= if (მოსწავლეს ნიშანია)=100 say good
-teacher say= if (მოსწავლეს ნიშანია)=90-80 say it normal
-teacher say= if (მოსწავლეს ნიშანია)=70-60 say bad
-teacher say= if (მოსწავლეს ნიშანია)=50-40 say perents in school
-
-დავალებაში უნდა დაგვეწერა ნიშნები რომელიც მიიღეს მოსწავლეებმა და მისი შესაფერისი სიყვა მაგალითად: good it normal და სხვა და ეს უნდა გამოეტანა ტრემინალს'
