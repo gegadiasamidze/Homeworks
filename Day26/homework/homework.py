48d303c248bb9e9b2ed756e135ea91e5ad1a6942
gegadiasamidze <gegadiasamidze2011@gmail.com>
2024-04-30 18:03:48 +0400

    commit

---
+++ Day26/homework/homework.py
@@ -0,0 +1,12 @@
+learn_goa = input("გოაში სწავლობ? ")
+
+if learn_goa == "კი":
+    mentor_group = input("რომელ ჯგუფში ხარ ")
+    
+    if mentor_group== 13:
+        name=input("არის შენი მენტორი გაბრიელი?")
+        if name == "კი":
+            print("შენ ნამდვილად ჯგუფ 13ში ხარ")
+        else:
+            print("შენ იტყუები")
+
