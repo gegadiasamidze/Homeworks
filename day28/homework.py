502300288ef675cc517840a67c7c8ab1e0a9bec6
gegadiasamidze <gegadiasamidze2011@gmail.com>
2024-05-10 21:35:05 +0400

    commit

---
+++ day28/homework.py
@@ -0,0 +1,7 @@
+family_members = ["გეგა", "თამუნა", "სულხანი", "ნუცა","გაბრიელი"]
+
+# ერთ გრძელ წინადადებაში დიდი სტრინგი
+family_string = "მე ვარ " + family_members[0] + ", დედაჩემია " + family_members[1] + ", მამაჩემია " + family_members[2] + "ჩემი და" + family_members[3] + "ჩემი ძმა" + family_members[4]
+
+# წინადადებას დავადგენთ
+print(family_string)
