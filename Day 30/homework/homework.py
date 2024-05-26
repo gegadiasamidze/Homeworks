bd0f3f7a8206c9a844ce88882522e400dff38ee0
gegadiasamidze <gegadiasamidze2011@gmail.com>
2024-05-26 17:55:02 +0400

    commit

---
+++ Day 30/homework/homework.py
@@ -0,0 +1,9 @@
+favorite_cars =["mersedes","buggati","lamborgini","bmw"]
+not_favorite_car=["nisani"]
+if not_favorite_car in favorite_cars:
+    index =favorite_cars.index(not_favorite_car)
+    favorita_cars[index]="porshe"
+
+    for car in favorita_cars:
+        print(car)
+        
